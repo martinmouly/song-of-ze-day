250edf134f3e9039ca7a0e2183c100be2e9ae925
Martin Mouly <martin@mouly.name>
2023-03-10 22:29:46 +0100

    add url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import spotifyLogo from './spotify-logo.png'
+import { useState } from 'react';
 
 function App() {
+  const buttonOnClick = () => {
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='button-5' onClick={buttonOnClick}>
+        <img src={spotifyLogo} className="spotify-logo"/>
+        Song of the day
+      </button>
     </div>
   );
 }
